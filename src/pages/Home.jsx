2c1e653e3a9c19d1ede043d6a051ca2e3368ddc1
Gaurav Sharma <gauravs_iet@yahoo.com>
2022-12-12 23:51:05 +0530

    new changes

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import UserResults from "../components/users/UserResults"
+import UserSearch from "../components/users/UserSearch"
+
+function Home() {
+  return (
+    <>
+    {/* {Search Component} */}
+    <UserSearch />
+    <UserResults />
+    </>
+  )
+}
+
+export default Home
